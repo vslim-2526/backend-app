8380bb0b5c4324ed71e75cc8e2cb48ec4bb13baf
dongphong543 <dongphong543@gmail.com>
2025-08-31 12:01:13 +0700

    Packages and configs

---
+++ config.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import path from "path";
+
+// Load .env before exporting anything
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+
+export const PORT = parseInt(process.env.PORT || "8000", 10);
+export const ENV = process.env.ENV || "DEV";
+export const MONGODB_ENDPOINT = process.env.MONGODB_ENDPOINT;
